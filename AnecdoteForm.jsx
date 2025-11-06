5bea20bcc61647ecc646cf185b0c1dd69c9bcd2e
Gladys Gopolang Masia <gladysgops@gmail.com>
2025-11-06 21:16:43 +0200

    Added a feature that shows an error message if the anecdote is too short (less than 5 characters)

--- AnecdoteForm.jsx
+++ AnecdoteForm.jsx
@@ -1,27 +1,49 @@
 
-import { useState } from 'react'
-import { useNotificationDispatch, setNotification } from './context/NotificationContext'
+import { useMutation, useQueryClient } from '@tanstack/react-query'
+import { useContext } from 'react'
+import { createAnecdote } from '../requests'
+import NotificationContext from '../context/NotificationContext'
 
-const AnecdoteForm = ({ addNew }) => {
-  const [content, setContent] = useState('')
-  const dispatch = useNotificationDispatch()
+const AnecdoteForm = () => {
+  const queryClient = useQueryClient()
+  const [notification, setNotification] = useContext(NotificationContext)
 
-  const handleSubmit = (event) => {
+  const newAnecdoteMutation = useMutation({
+    mutationFn: createAnecdote,
+    onSuccess: (newAnecdote) => {
+      const anecdotes = queryClient.getQueryData(['anecdotes'])
+      queryClient.setQueryData(['anecdotes'], anecdotes.concat(newAnecdote))
+      setNotification(`Anecdote '${newAnecdote.content}' added!`)
+      setTimeout(() => setNotification(''), 5000)
+    },
+    onError: (error) => {
+      setNotification(error.response.data.error)
+      setTimeout(() => setNotification(''), 5000)
+    },
+  })
+
+  const onCreate = (event) => {
     event.preventDefault()
-    addNew(content)
-    setNotification(dispatch, `You added "${content}"`, 5)
-    setContent('')
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+
+    if (content.length < 5) {
+      setNotification('too short anecdote, must have length 5 or more')
+      setTimeout(() => setNotification(''), 5000)
+      return
+    }
+
+    newAnecdoteMutation.mutate({ content, votes: 0 })
   }
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h3>Add a new anecdote</h3>
-      <input
-        value={content}
-        onChange={(e) => setContent(e.target.value)}
-      />
-      <button type="submit">create</button>
-    </form>
+    <div>
+      <h3>create new</h3>
+      <form onSubmit={onCreate}>
+        <input name="anecdote" />
+        <button type="submit">create</button>
+      </form>
+    </div>
   )
 }
 
