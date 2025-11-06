787e369a348533b67678c8e364f643f671548877
Gladys Gopolang Masia <gladysgops@gmail.com>
2025-11-06 20:52:58 +0200

    Fixed App.jsx to use NotificationProvider correctly

--- App.jsx
+++ App.jsx
@@ -1,17 +1,18 @@
+
 import { useReducer, useEffect } from 'react'
 import Notification from './Notification'
-import { NotificationContextProvider } from './Context/NotificationContext'
+import { NotificationProvider } from './Context/NotificationContext'
 
 const App = () => {
   return (
-    <NotificationContextProvider>
+    <NotificationProvider>
       <div>
         <h2>Anecdote App</h2>
         <Notification />
         <p>If it hurts, do it more often</p>
         <button>vote</button>
       </div>
-    </NotificationContextProvider>
+    </NotificationProvider>
   )
 }
 
